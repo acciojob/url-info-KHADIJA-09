8bd9cb6e796e106075440b1467fb48cd9e2df9d2
KHADIJA-09 <132650876+KHADIJA-09@users.noreply.github.com>
2025-01-21 11:21:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-const curr =window.location.url
-alert("The length of the URL is:",curr.length)
+//your JS code here. If required.
+const currentURL = window.location.href;
 
+// Count the number of characters in the URL using the length property
+const urlLength = currentURL.length;
+
+// Display the length of the URL in an alert box
+alert(`The length of the URL is: ${urlLength}`);
